5e883a4e7a3c433bc62af9ae783d2b23172da595
sprindjack <17216401+sprindjack@users.noreply.github.com>
2021-08-29 15:09:09 +0800

    Create completeanat.js

---
+++ res/completeanat.js
@@ -0,0 +1,37 @@
+
+
+var obj= {
+  "upgrades": [],
+  "subscriptions": [],
+  "content": {},
+  "bypass": {
+    "expire": "2099-09-01 06:48:19",
+    "comment": "ca19",
+    "active": true,
+    "bypass_type": "ca19",
+    "expire_ts": "4091899699"
+  },
+  "settings": {},
+  "check": {
+    "receipt_type": "Production",
+    "adam_id": 1054948424,
+    "app_item_id": 1054948424,
+    "bundle_id": "com.3D4Medical.CA",
+    "application_version": "14657",
+    "download_id": 500668371053414900,
+    "version_external_identifier": 843360153,
+    "receipt_creation_date": "2021-08-29 06:57:42 Etc/GMT",
+    "receipt_creation_date_ms": "1630220262000",
+    "receipt_creation_date_pst": "2021-08-28 23:57:42 America/Los_Angeles",
+    "request_date": "2021-08-29 06:57:46 Etc/GMT",
+    "request_date_ms": "1630220266344",
+    "request_date_pst": "2021-08-28 23:57:46 America/Los_Angeles",
+    "original_purchase_date": "2021-08-29 06:51:48 Etc/GMT",
+    "original_purchase_date_ms": "1630219908000",
+    "original_purchase_date_pst": "2021-08-28 23:51:48 America/Los_Angeles",
+    "original_application_version": "14657",
+    "in_app": []
+  }
+  $done({body: JSON.stringify(obj)});
+  
+  
