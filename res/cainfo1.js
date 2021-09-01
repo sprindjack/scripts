367bff19c7c474f61790ad2e501a95ba2319c721
sprindjack <17216401+sprindjack@users.noreply.github.com>
2021-09-01 16:29:58 +0800

    Create cainfo1.js

---
+++ res/cainfo1.js
@@ -0,0 +1,144 @@
+var obj= {
+  "last_name" : "",
+  "id" : 51634,
+  "is_test_user" : true,
+  "bypasses" : [
+    {
+      "expire_ts" : "4096070058",
+      "expire" : "2099-10-19 05:14:18",
+      "active" : true,
+      "comment" : "ca19",
+      "bypass_type" : "ca19"
+    }
+  ],
+  "locale" : "zh-hans",
+  "location" : {
+
+  },
+  "preferences" : {
+    "uiFeatures" : {
+      "record" : false,
+      "tabs" : false,
+      "tools" : true,
+      "save_image" : true,
+      "tips" : true,
+      "grids" : true,
+      "ar" : true,
+      "save_screen" : true
+    },
+    "features" : {
+      "model_updates" : true,
+      "hub" : true,
+      "beating_heart" : true,
+      "auto_sync" : true,
+      "free_rotation" : false,
+      "publicity" : true
+    },
+    "toolsOrder" : [
+      1,
+      27,
+      4,
+      5,
+      7,
+      32,
+      31,
+      3,
+      28,
+      6,
+      19,
+      20,
+      18,
+      22,
+      29,
+      30,
+      23,
+      24,
+      25,
+      26
+    ],
+    "theme" : 0,
+    "version" : 1.3
+  },
+  "check" : [
+
+  ],
+  "is_hidden" : false,
+  "image_url" : "",
+  "modified_timestamp" : "2021-08-29 06:48:19",
+  "interests" : [
+
+  ],
+  "biography" : "",
+  "licenses" : [
+    {
+      "bundle" : "ca19",
+      "expire" : "2099-10-19T05:14:18.874810Z",
+      "active" : true,
+      "source" : null,
+      "offer" : "",
+      "in_billing_retry" : false,
+      "store" : "manual"
+    }
+  ],
+  "permissions" : {
+    "embeds_maker" : true,
+    "lecture_maker" : true,
+    "lecture_reviewer" : true,
+    "video_converter" : true,
+    "recording_publisher" : true
+  },
+  "email" : "abc@abc.com",
+  "study_topic" : 0,
+  "subscriptions" : [
+
+  ],
+  "bypass" : {
+    "expire_ts" : "4096070058",
+    "expire" : "2099-10-19 05:14:18 Etc/GMT",
+    "active" : true,
+    "comment" : "ca19",
+    "bypass_type" : "ca19"
+  },
+  "organization" : null,
+  "settings" : {
+
+  },
+  "username" : "郑 ",
+  "show_content_update" : true,
+  "study_year" : 0,
+  "last_password_change" : null,
+  "first_name" : "",
+  "institution" : "",
+  "content" : {
+
+  },
+  "kind" : "Healthcare Provider",
+  "upgrades" : [
+
+  ],
+  "last_login" : "2021-08-29 06:55:51",
+  "organization_image_url" : "",
+  "study_stage" : 0,
+  "title" : "-",
+  "sessions" : {
+    "ca" : 0,
+    "phone" : 0,
+    "mac" : 0,
+    "cb" : 0,
+    "course_system" : 0,
+    "win" : 0,
+    "course_region" : 0,
+    "ipad" : 1,
+    "android" : 0
+  },
+  "study_show_popup" : false,
+  "limits" : {
+    "plates" : 200,
+    "snippets" : 50,
+    "quizzes" : 200,
+    "lectures" : -1,
+    "groups" : 5,
+    "questions" : 200
+  }  
+};
+$done({body: JSON.stringify(obj)});
