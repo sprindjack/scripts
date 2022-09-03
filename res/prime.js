7081f688b7a9f7da4c907978678f2cba359f941f
sprindjack <17216401+sprindjack@users.noreply.github.com>
2022-09-03 19:06:47 +0800

    Update prime.js

--- res/prime.js
+++ res/prime.js
@@ -2,6 +2,4 @@ let obj = JSON.parse($response.body);
 obj.status = 0;
 obj.code = 0;
 obj.expiration_time = 4101790766;
-obj.email = "test@test";
-obj.linked = 1;
 $done({body: JSON.stringify(obj)});
